068a1a5775a8e84b3fc6f7780b14a5b48d9cef6a
peddintinandini <96322585+peddintinandini@users.noreply.github.com>
2022-10-22 21:07:23 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int a[n],i,j,c;     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for (i=0;i<n;i++)     {         c=0;         for (j=0;j<n;j++)         {             if (i!=j)             {                 {                     if( a[i]==a[j])                     {                        c=c+1 ;                     }                 }
                }
            }
            
        
    if (c==1)
        {
            continue;
        }
     else
        {
            printf("%d",a[i]);
            break;
        }
        
    }
    }

---
+++ Single_Number.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n],i,j,c;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for (i=0;i<n;i++)
+    {
+        c=0;
+        for (j=0;j<n;j++)
+        {
+            if (i!=j)
+            {
+                {
+                    if( a[i]==a[j])
+                    {
+                       c=c+1 ;
+                    }
+                }
+                
+            }
+        }
+        
+    
+if (c==1)
+    {
+        continue;
+    }
+ else
+    {
+        printf("%d",a[i]);
+        break;
+    }
+    
+}
+}
